5a2df898669b80aa4ed11da8124a8437bc22244a
summer <1065300945@qq.com>
2021-01-10 22:04:20 +0800

    first commit

---
+++ Migrations/20210110135151_Init.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace MySQL_Demo.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Musics",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Game = table.Column<string>(nullable: true),
+                    PublishTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musics", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Singers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Singers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Musics");
+
+            migrationBuilder.DropTable(
+                name: "Singers");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore;
+
+namespace MySQL_Demo
+{
+    public class MyAppDbContext : DbContext
+    {
+        public DbSet<Music> Musics{get;set;}
+        public DbSet<Singer> Singers{get;set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            options.UseMySQL(@"Server=localhost;
+            port=3306;
+            Database=test;
+            Uid=root;
+            Pwd=root;
+            Allow User Variables=True;
+            sslMode=None;");
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    public class Music
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Game { get; set; }
+        public DateTime PublishTime { get; set; }
+    }
+
+    public class Singer
+    {
+        public int Id  { get; set; }
+        public string Name { get; set; }
+    }
+}
